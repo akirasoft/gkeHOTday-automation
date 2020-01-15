d003c91860718896f23609022fe85a354f03ad3a
Michael Villiger <akirasoft@gmail.com>
2020-01-15 13:22:26 -0600

    updates

--- docker-image/cluster-create-wrapper.sh
+++ docker-image/cluster-create-wrapper.sh
@@ -13,10 +13,18 @@ for ((i=1;i<=NUM_OF_CLUSTERS;i++)); do
     # cluster-create.sh creates the cluster
     CLUSTER_NAME="${CLUSTER_PREFIX}${i}"
     ./cluster-create.sh ${PROJECT} ${CLUSTER_NAME} ${ZONE} ${REGION} ${GKE_VERSION} 
+done
+# need second logic block for kubeconfig generation so we don't have to wait inline for all the clusters
+for ((i=1;i<=NUM_OF_CLUSTERS;i++)); do
+    CLUSTER_NAME="${CLUSTER_PREFIX}${i}"
     # get-kubeconfig creates and uploads cert to remove gcloud pre-req for k8s auth
     # creates kubeconfig file in build dir
-    sleep 60
-    kubectl cluster-info
+    # when 4 ocntainers are ready, begin kubeconfig creation
+    until [[ kubectl get pods -n kube-system -l k8s-app=kube-dns -o jsonpath='{.items[*].status.phase}' == "Ready" ]];
+    do
+        echo "cluster still provisioning, waiting 30 seconds"
+        sleep 30
+    done
+    echo "cluster created, now enabling access"
     ./get-kubeconfig.sh ${CLUSTER_NAME}
 done
-
